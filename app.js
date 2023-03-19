1ab8cc99e6bcdf33fd0d7d0fc602c77f0a86caaf
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-03-19 09:40:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 "use strict";
 
-// ============ Global Variables ============ //
-const potterName = "Harry Potter";
-const potterGender = "male";
-const potterHouse = "Gryffindor";
-const potterDateOfBirth = "31-07-1980";
-const potterAncestry = "half-blood";
-const potterEyeColour = "green";
-const potterHairColour = "black";
-const potterActor = "Daniel Radcliffe";
-const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
-
 // ============ LOAD & INIT APP ============ //
 window.addEventListener("load", initApp);
 
 function initApp() {
+    // Harry Potter
+    const potterName = "Harry Potter";
+    const potterGender = "male";
+    const potterHouse = "Gryffindor";
+    const potterDateOfBirth = "31-07-1980";
+    const potterAncestry = "half-blood";
+    const potterEyeColour = "green";
+    const potterHairColour = "black";
+    const potterActor = "Daniel Radcliffe";
+    const potterImage = "http://hp-api.herokuapp.com/images/harry.jpg";
+
     console.log(
         potterName,
         potterGender,
